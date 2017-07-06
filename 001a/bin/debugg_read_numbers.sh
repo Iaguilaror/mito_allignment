54264f9392b1bbbd90eacf9c514bc4b89e0d19fe
´Israel <iaguilaror@gmail.com>
2017-07-06 10:48:05 -0500

    fixed for read count of unpaired reads

--- 001a/bin/debugg_read_numbers.sh
+++ 001a/bin/debugg_read_numbers.sh
@@ -3,7 +3,6 @@
 SAMPLE=$(grep "^TEST_SAMPLE" bin/testing_mk.sh | cut -d"\"" -f2)
 
 echo "[DEBUGGING]counts for $SAMPLE"
-CHRM_lines=$(wc -l results/$SAMPLE.unpaired.chrM.fastq | cut -d" " -f1)
-UNMAPPED_lines=$(wc -l results/$SAMPLE.unpaired.unmapped.fastq | cut -d" " -f1)
-echo "chrM.fastq.gz unpaired lines= $(( CHRM_lines / 4 ))"
-echo "unmapped.fastq.gz unpaired lines= $(( UNMAPPED_lines / 4 ))"
+FASTQ_lines=$(zcat results/formito_$SAMPLE.unpaired.fastq.gz | wc -l)
+echo "sample.fastq.gz unpaired lines= $(( FASTQ_lines / 4 ))"
+
